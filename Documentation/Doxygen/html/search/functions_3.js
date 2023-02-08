4d8ea59599cf8ef802bb9e29924e80de6bd2aaf8
Franzelfx <fabian.franz0596@gmail.com>
2023-02-08 13:47:08 +0100

    REMOVED latex sources

--- Documentation/Doxygen/html/search/functions_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['readregister_0',['readRegister',['../main_8hpp.html#aa6db46255cb370c4b02a204daefb40c2',1,'main.cpp']]],
-  ['rx_1',['rx',['../main_8hpp.html#a945deb388e5928de12dd7f6c2181b93a',1,'main.cpp']]]
-];
--- Documentation/Doxygen/html/search/functions_5.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['testi2c_5fread_0',['testI2C_read',['../main_8hpp.html#af3a3ef29e3e54bf2d1f3cd58a28db141',1,'main.cpp']]],
-  ['testleds_1',['testLeds',['../main_8hpp.html#a905c049550cebedf53cc88061f7e4b10',1,'main.cpp']]],
-  ['tx_2',['tx',['../main_8hpp.html#aee50f855dff840a3ce9870f6e289365e',1,'main.cpp']]]
-];
